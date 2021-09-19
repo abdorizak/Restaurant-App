8af5b8ec4eb2ed480664b3826266ac38dc4a88db
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-09-19 10:45:38 +0300

    Initial Commit

---
+++ Resturant App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Resturant App
+//
+//  Created by Abdirizak Hassan on 9/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
